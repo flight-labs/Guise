8d68730f77d91fae95fa4151f6c2ecda1e7c887f
Gregory Higley <code@revolucent.net>
2016-03-07 03:19:37 -0500

    Unit tests

--- GuiseTests/GuiseTests.swift
+++ GuiseTests/GuiseTests.swift
@@ -14,8 +14,12 @@ class GuiseTests: XCTestCase {
     override class func setUp() {
         super.setUp()
         DependencyResolver.register{ ServerCommunicator(widgetCount: 3) as ServerCommunicating }
-        DependencyResolver.register(name: "sc2") { ServerCommunicator(widgetCount: 7) as ServerCommunicating }
+        DependencyResolver.register(name: "sc2") { () -> ServerCommunicating in
+            let eight = DependencyResolver.resolve(name: "8")! as Int
+            return ServerCommunicator(widgetCount: eight)
+        }
         DependencyResolver.register(name: "sc3") { (count: Int) in ServerCommunicator(widgetCount: count) as ServerCommunicating }
+        DependencyResolver.register(8, name: "8") // Any type can be registered
     }
     
     func testResolveServerCommunicator() {
@@ -25,12 +29,17 @@ class GuiseTests: XCTestCase {
     
     func testResolveNamedServerCommunicator() {
         let serverCommunicator = DependencyResolver.resolve(name: "sc2")! as ServerCommunicating
-        XCTAssertEqual(serverCommunicator.retrieveWidgetCount(), 7)
+        XCTAssertEqual(serverCommunicator.retrieveWidgetCount(), 8)
     }
     
     func testResolveParameterizedNamedServerCommunicator() {
         let count = 9
         let serverCommunicator = DependencyResolver.resolve(count, name: "sc3")! as ServerCommunicating
         XCTAssertEqual(serverCommunicator.retrieveWidgetCount(), count)
+    }
+    
+    func testResolveInt() {
+        let eight = DependencyResolver.resolve(name: "8")! as Int
+        XCTAssertEqual(eight, 8)
     }
 }
