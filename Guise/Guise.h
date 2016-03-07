bbf43df773e5f98c247fd2fdafed42386e109854
Gregory Higley <code@revolucent.net>
2016-03-07 02:28:56 -0500

    Initial commit

---
+++ Guise/Guise.h
@@ -0,0 +1,19 @@
+//
+//  Guise.h
+//  Guise
+//
+//  Created by Gregory Higley on 3/7/16.
+//  Copyright © 2016 Gregory Higley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Guise.
+FOUNDATION_EXPORT double GuiseVersionNumber;
+
+//! Project version string for Guise.
+FOUNDATION_EXPORT const unsigned char GuiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Guise/PublicHeader.h>
+
+
