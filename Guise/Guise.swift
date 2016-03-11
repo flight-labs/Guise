47b349be014a62dd48774a5c810384b362689b6a
Gregory Higley <code@revolucent.net>
2016-03-11 10:33:51 -0500

    Lifecycle, unregistration

--- Guise/Guise.swift
+++ Guise/Guise.swift
@@ -24,9 +24,21 @@ SOFTWARE.
 
 import Foundation
 
+/**
+ Guise dependency lifecycle.
+*/
 public enum Lifecycle {
+    /**
+     The registered block is called on every resolution.
+    */
     case NotCached
+    /**
+     The registered block is invoked only once. After that, its cached result is returned.
+    */
     case Cached
+    /**
+     The dependency is completely removed after it is resolved the first time.
+    */
     case OneTime
 }
 
@@ -60,24 +72,24 @@ public struct Guise {
     }
     
     private class Dependency {
-        private let create: Any -> Any
+        private let eval: Any -> Any
         private let lifecycle: Lifecycle
         private var instance: Any?
         
-        init<P, D>(lifecycle: Lifecycle, create: P -> D) {
+        init<P, D>(lifecycle: Lifecycle, eval: P -> D) {
             self.lifecycle = lifecycle
-            self.create = { param in create(param as! P) }
+            self.eval = { param in eval(param as! P) }
         }
         
-        func resolve<P, D>(parameters: P, cached: Bool) -> D {
+        func resolve<P, D>(parameters: P, lifecycle: Lifecycle) -> D {
             var result: D
-            if cached && self.lifecycle == .Cached {
+            if lifecycle != .NotCached && self.lifecycle == .Cached {
                 if instance == nil {
-                    instance = create(parameters)
+                    instance = eval(parameters)
                 }
                 result = instance! as! D
             } else {
-                result = create(parameters) as! D
+                result = eval(parameters) as! D
             }
             return result
         }
@@ -88,18 +100,18 @@ public struct Guise {
     private init() {}
     
     /**
-     Registers `create` with Guise.
+     Registers `eval` with Guise.
      
      - parameter type: Usually the type of `D`, but can be any string.
      - parameter name: An optional name to disambiguate similar `type`s.
-     - parameter cached: Whether or not the instance is lazily created and then cached.
-     - parameter create: The lambda to register with Guise.
+     - parameter lifecycle: The lifecyle of the registered dependency.
+     - parameter eval: The block to register with Guise.
      
      - returns: The registration key, which can be used with `unregister`.
      */
-    public static func register<P, D>(type type: String = String(reflecting: D.self), name: String? = nil, lifecycle: Lifecycle = .NotCached, create: P -> D) -> Any {
+    public static func register<P, D>(type type: String = String(reflecting: D.self), name: String? = nil, lifecycle: Lifecycle = .NotCached, eval: P -> D) -> Any {
         let key = Key(type: type, name: name)
-        dependencies[key] = Dependency(lifecycle: lifecycle, create: create)
+        dependencies[key] = Dependency(lifecycle: lifecycle, eval: eval)
         return key
     }
     
@@ -107,7 +119,7 @@ public struct Guise {
      Registers an existing instance with Guise.
      
      - note: This effectively creates a singleton. If you want your singleton created lazily,
-     register it with a lambda and set `cached` to true.
+     register it with a block and set `cached` to true.
      
      - parameter instance: The instance to register.
      - parameter type: Usually the type of `D`, but can be any string.
@@ -122,42 +134,48 @@ public struct Guise {
     /**
      Resolves an instance of `D` in Guise.
      
-     - parameter parameters: The parameters to pass to the registered lambda.
+     - parameter parameters: The parameters to pass to the registered block.
      - parameter type: Usually the type of `D`, can be any string.
-     - parameter name: An optional name to disambiguate similar `type`s.
+     - parameter lifecycle: The lifecyle of the registered dependency.
      - parameter cached: Prefer a cached value if available.
      
-     - returns: The result of the registered lambda, or nil if not registered.
+     - returns: The result of the registered block, or nil if not registered.
      
-     - note: The value of `cached` is meaningful only if parallelled by a previous
-     registration where `cached` was set to `true`. Otherwise it has no effect.
+     - note: The meaning of `lifecycle` here is a bit complex. If `.OneTime` is passed, the dependency is returned and
+     then immediately unregistered, regardless of how it was originally registered. If .Cached (the default) is passed and
+     the dependency was originally registered with .Cached, a cached a value is returned. If .NotCached is passed and the
+     dependency was originally registered with .Cached, the cached value is ignored and a new value is calculated. In all
+     other cases, a new value is calculated by invoking the registered block.
      */
-    public static func resolve<P, D>(parameters: P, type: String = String(reflecting: D.self), name: String? = nil, cached: Bool = true) -> D? {
+    public static func resolve<P, D>(parameters: P, type: String = String(reflecting: D.self), name: String? = nil, lifecycle: Lifecycle = .Cached) -> D? {
         let key = Key(type: type, name: name)
         guard let dependency = dependencies[key] else { return nil }
         defer {
-            if dependency.lifecycle == .OneTime {
+            if lifecycle == .OneTime || dependency.lifecycle == .OneTime {
                 unregister(key)
             }
         }
-        return (dependency.resolve(parameters, cached: cached) as D)
+        return (dependency.resolve(parameters, lifecycle: lifecycle) as D)
     }
     
     /**
      Resolves an instance of `D` in Guise.
      
-     - parameter parameters: The parameters to pass to the registered lambda.
+     - parameter parameters: The parameters to pass to the registered block.
      - parameter type: Usually the type of `D`, can be any string.
      - parameter name: An optional name to disambiguate similar `type`s.
      - parameter cached: Prefer a cached value if available.
      
-     - returns: The result of the registered lambda, or nil if not registered.
+     - returns: The result of the registered block, or nil if not registered.
      
-     - note: The value of `cached` is meaningful only if parallelled by a previous
-     registration where `cached` was set to `true`. Otherwise it has no effect.
+     - note: The meaning of `lifecycle` here is a bit complex. If `.OneTime` is passed, the dependency is returned and
+     then immediately unregistered, regardless of how it was originally registered. If .Cached (the default) is passed and
+     the dependency was originally registered with .Cached, a cached a value is returned. If .NotCached is passed and the
+     dependency was originally registered with .Cached, the cached value is ignored and a new value is calculated. In all
+     other cases, a new value is calculated by invoking the registered block.
      */
-    public static func resolve<D>(type type: String = String(reflecting: D.self), name: String? = nil, cached: Bool = true) -> D? {
-        return resolve((), type: type, name: name, cached: cached)
+    public static func resolve<D>(type type: String = String(reflecting: D.self), name: String? = nil, lifecycle: Lifecycle = .Cached) -> D? {
+        return resolve((), type: type, name: name, lifecycle: lifecycle)
     }
     
     /**
@@ -186,17 +204,17 @@ public struct Guise {
     }
     
     /**
-     Unregisters the dependency with the given name and type, as determined by the block.
+     Unregisters the dependency with the given name and type, as determined by the `eval` block.
      
      - parameter name: The optional name under which the dependency was registered.
-     - parameter create: A block (not called) used to determine the type that was registered.
+     - parameter eval: A block (not called) used to determine the type that was registered.
      
      - returns: Whether or not the key was present and could be unregistered.
      
      - note: The block is never called. It is only used to determine the type used to
      originally register the block.
     */
-    public static func unregister<D>(name: String? = nil, create: () -> D) -> Bool {
+    public static func unregister<P, D>(name: String? = nil, eval: P -> D) -> Bool {
         return unregister(type: String(reflecting: D.self), name: name)
     }
     
