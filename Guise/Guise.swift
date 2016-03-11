50cccac5c5d2456cd6a75329d4e926bf466cce47
Gregory Higley <code@revolucent.net>
2016-03-11 09:39:03 -0500

    Better lifecycle management

--- Guise/Guise.swift
+++ Guise/Guise.swift
@@ -24,6 +24,12 @@ SOFTWARE.
 
 import Foundation
 
+public enum Lifecycle {
+    case NotCached
+    case Cached
+    case OneTime
+}
+
 /**
  A simple, elegant, flexible dependency resolver.
  
@@ -55,17 +61,17 @@ public struct Guise {
     
     private class Dependency {
         private let create: Any -> Any
-        private let cached: Bool
+        private let lifecycle: Lifecycle
         private var instance: Any?
         
-        init<P, D>(cached: Bool, create: P -> D) {
-            self.cached = cached
+        init<P, D>(lifecycle: Lifecycle, create: P -> D) {
+            self.lifecycle = lifecycle
             self.create = { param in create(param as! P) }
         }
         
         func resolve<P, D>(parameters: P, cached: Bool) -> D {
             var result: D
-            if cached && self.cached {
+            if cached && self.lifecycle == .Cached {
                 if instance == nil {
                     instance = create(parameters)
                 }
@@ -88,10 +94,13 @@ public struct Guise {
      - parameter name: An optional name to disambiguate similar `type`s.
      - parameter cached: Whether or not the instance is lazily created and then cached.
      - parameter create: The lambda to register with Guise.
+     
+     - returns: The registration key, which can be used with `unregister`.
      */
-    public static func register<P, D>(type type: String = String(reflecting: D.self), name: String? = nil, cached: Bool = false, create: P -> D) {
+    public static func register<P, D>(type type: String = String(reflecting: D.self), name: String? = nil, lifecycle: Lifecycle = .NotCached, create: P -> D) -> Any {
         let key = Key(type: type, name: name)
-        dependencies[key] = Dependency(cached: cached, create: create)
+        dependencies[key] = Dependency(lifecycle: lifecycle, create: create)
+        return key
     }
     
     /**
@@ -103,9 +112,11 @@ public struct Guise {
      - parameter instance: The instance to register.
      - parameter type: Usually the type of `D`, but can be any string.
      - parameter name: An optional name to disambiguate similar `type`s.
+     
+     - returns: The registration key, which can be used with `unregister`.
      */
-    public static func register<D>(instance: D, type: String = String(reflecting: D.self), name: String? = nil) {
-        register(type: type, name: name, cached: true) { instance }
+    public static func register<D>(instance: D, type: String = String(reflecting: D.self), name: String? = nil) -> Any {
+        return register(type: type, name: name, lifecycle: .Cached) { instance }
     }
     
     /**
@@ -124,6 +135,11 @@ public struct Guise {
     public static func resolve<P, D>(parameters: P, type: String = String(reflecting: D.self), name: String? = nil, cached: Bool = true) -> D? {
         let key = Key(type: type, name: name)
         guard let dependency = dependencies[key] else { return nil }
+        defer {
+            if dependency.lifecycle == .OneTime {
+                unregister(key)
+            }
+        }
         return (dependency.resolve(parameters, cached: cached) as D)
     }
     
@@ -144,6 +160,52 @@ public struct Guise {
         return resolve((), type: type, name: name, cached: cached)
     }
     
+    /**
+     Unregisters the dependency with the given key.
+     
+     - returns: Whether or not the key was present and could be unregistered.
+    */
+    public static func unregister(key: Any) -> Bool {
+        if let key = key as? Key {
+            return dependencies.removeValueForKey(key) != nil
+        }
+        return false
+    }
+
+    /**
+     Unregisters the dependency with the given type and name.
+     
+     - parameter type: The type of the dependency to unregister.
+     - parameter name: The name of the dependency to unregister (optional).
+     
+     - returns: Whether or not the key was present and could be unregistered.
+    */
+    public static func unregister(type type: String, name: String? = nil) -> Bool {
+        let key = Key(type: type, name: name)
+        return unregister(key)
+    }
+    
+    /**
+     Unregisters the dependency with the given name and type, as determined by the block.
+     
+     - parameter name: The optional name under which the dependency was registered.
+     - parameter create: A block (not called) used to determine the type that was registered.
+     
+     - returns: Whether or not the key was present and could be unregistered.
+     
+     - note: The block is never called. It is only used to determine the type used to
+     originally register the block.
+    */
+    public static func unregister<D>(name: String? = nil, create: () -> D) -> Bool {
+        return unregister(type: String(reflecting: D.self), name: name)
+    }
+    
+    /**
+     Clears all dependencies from Guise.
+    */
+    public static func reset() {
+        dependencies = [:]
+    }
 }
 
 private func ==(lhs: Guise.Key, rhs: Guise.Key) -> Bool {
