ff940281e99248505bfd9dc78a58e2487cb5e809
Gregory Higley <code@revolucent.net>
2017-07-14 14:35:50 -0400

    Playing around with weak registrations

--- GuiseTests/Guise.playground/Contents.swift
+++ GuiseTests/Guise.playground/Contents.swift
@@ -3,9 +3,20 @@
 import Cocoa
 import Guise
 
+protocol Zing: class {}
+class Watusi: Zing {}
+
+let watusi = Watusi()
+_ = Guise.register{ [weak watusi] in watusi as Zing? }
+if Guise.exists(type: Zing?.self, name: Name.default, container: Name.default), let w = Guise.resolve()! as Zing? {
+    w
+}
+
 _ = Guise.register(name: UUID(), container: Container.plugin, metadata: PluginType.editor) { Plugin1() as Plugin }
 _ = Guise.register(name: UUID(), container: Container.plugin, metadata: PluginType.viewer) { Plugin2() as Plugin }
 _ = Guise.register(name: UUID(), container: Container.plugin, metadata: PluginType.viewer) { Plugin3() as Plugin }
 
 var viewerKeys = Guise.filter(type: Plugin.self, metadata: PluginType.viewer)
+
+let viewers = Guise.resolve(keys: viewerKeys) as [Plugin]
 
