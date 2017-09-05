5f1c58a20784347223609c3e910f0f034f0f2e9c
Gregory Higley <code@revolucent.net>
2017-09-05 07:42:57 -0400

    Streamlined the core `filter` method by eliminating superfluous `type` parameter.

--- Guise/Filtering.swift
+++ Guise/Filtering.swift
@@ -41,10 +41,16 @@ extension Guise {
 extension Guise {
     
     /// All roads lead here
-    private static func filter<K: Keyed & Hashable>(type: String?, name: AnyHashable?, container: AnyHashable?, metathunk: Metathunk?) -> Set<K> {
+    private static func filter<K: Keyed & Hashable>(name: AnyHashable?, container: AnyHashable?, metathunk: Metathunk?) -> Set<K> {
         let keymap: (Dictionary<AnyKey, Dependency>.Element) -> Dictionary<K, Dependency>.Element? = {
+            /*
+             Filtering by type occurs when `K` is `Key<T>`. For instance, if `K` is `Key<String>` but `$0.key.type` is
+             `Swift.Int`, the initializer will fail because of the type incompatibility. This is why no explicit `type`
+             parameter for this method is needed.
+             
+             When *not* filtering by type, `K` should be `AnyKey`.
+             */
             guard let key = K($0.key) else { return nil }
-            if let type = type, type != key.type { return nil }
             if let name = name, name != key.name { return nil }
             if let container = container, container != key.container { return nil }
             return (key: key, value: $0.value)
@@ -59,11 +65,11 @@ extension Guise {
     // MARK: Filter By Type, Name, & Container
     
     public static func filter<T, N: Hashable, C: Hashable>(type: T.Type, name: N, container: C) -> Key<T>? {
-        return filter(type: nil, name: name, container: container, metathunk: nil).first
+        return filter(name: name, container: container, metathunk: nil).first
     }
     
     public static func filter<T, N: Hashable, C: Hashable, M>(type: T.Type, name: N, container: C, metafilter: @escaping Metafilter<M>) -> Key<T>? {
-        return filter(type: nil, name: name, container: container, metathunk: metathunk(metafilter)).first
+        return filter(name: name, container: container, metathunk: metathunk(metafilter)).first
     }
     
     public static func filter<T, N: Hashable, C: Hashable, M: Equatable>(type: T.Type, name: N, container: C, metadata: M) -> Key<T>? {
@@ -73,11 +79,11 @@ extension Guise {
     // MARK: Filter By Type & Name
     
     public static func filter<T, N: Hashable>(type: T.Type, name: N) -> Set<Key<T>> {
-        return filter(type: nil, name: name, container: nil, metathunk: nil)
+        return filter(name: name, container: nil, metathunk: nil)
     }
     
     public static func filter<T, N: Hashable, M>(type: T.Type, name: N, metafilter: @escaping Metafilter<M>) -> Set<Key<T>> {
-        return filter(type: nil, name: name, container: nil, metathunk: metathunk(metafilter))
+        return filter(name: name, container: nil, metathunk: metathunk(metafilter))
     }
     
     public static func filter<T, N: Hashable, M: Equatable>(type: T.Type, name: N, metadata: M) -> Set<Key<T>> {
@@ -87,11 +93,11 @@ extension Guise {
     // MARK: Filter By Type & Container
     
     public static func filter<T, C: Hashable>(type: T.Type, container: C) -> Set<Key<T>> {
-        return filter(type: nil, name: nil, container: container, metathunk: nil)
+        return filter(name: nil, container: container, metathunk: nil)
     }
     
     public static func filter<T, C: Hashable, M>(type: T.Type, container: C, metafilter: @escaping Metafilter<M>) -> Set<Key<T>> {
-        return filter(type: nil, name: nil, container: container, metathunk: metathunk(metafilter))
+        return filter(name: nil, container: container, metathunk: metathunk(metafilter))
     }
     
     public static func filter<T, C: Hashable, M: Equatable>(type: T.Type, container: C, metadata: M) -> Set<Key<T>> {
@@ -101,11 +107,11 @@ extension Guise {
     // MARK: Filter By Type
     
     public static func filter<T>(type: T.Type) -> Set<Key<T>> {
-        return filter(type: nil, name: nil, container: nil, metathunk: nil)
+        return filter(name: nil, container: nil, metathunk: nil)
     }
     
     public static func filter<T, M>(type: T.Type, metafilter: @escaping Metafilter<M>) -> Set<Key<T>> {
-        return filter(type: nil, name: nil, container: nil, metathunk: metathunk(metafilter))
+        return filter(name: nil, container: nil, metathunk: metathunk(metafilter))
     }
     
     public static func filter<T, M: Equatable>(type: T.Type, metadata: M) -> Set<Key<T>> {
@@ -115,11 +121,11 @@ extension Guise {
     // MARK: Filter By Name & Container
     
     public static func filter<N: Hashable, C: Hashable>(name: N, container: C) -> Set<AnyKey> {
-        return filter(type: nil, name: name, container: container, metathunk: nil)
+        return filter(name: name, container: container, metathunk: nil)
     }
     
     public static func filter<N: Hashable, C: Hashable, M>(name: N, container: C, metafilter: @escaping Metafilter<M>) -> Set<AnyKey> {
-        return filter(type: nil, name: name, container: container, metathunk: metathunk(metafilter))
+        return filter(name: name, container: container, metathunk: metathunk(metafilter))
     }
     
     public static func filter<N: Hashable, C: Hashable, M: Equatable>(name: N, container: C, metadata: M) -> Set<AnyKey> {
@@ -129,11 +135,11 @@ extension Guise {
     // MARK: Filter By Name
     
     public static func filter<N: Hashable>(name: N) -> Set<AnyKey> {
-        return filter(type: nil, name: name, container: nil, metathunk: nil)
+        return filter(name: name, container: nil, metathunk: nil)
     }
     
     public static func filter<N: Hashable, M>(name: N, metafilter: @escaping Metafilter<M>) -> Set<AnyKey> {
-        return filter(type: nil, name: name, container: nil, metathunk: metathunk(metafilter))
+        return filter(name: name, container: nil, metathunk: metathunk(metafilter))
     }
     
     public static func filter<N: Hashable, M: Equatable>(name: N, metadata: M) -> Set<AnyKey> {
@@ -143,11 +149,11 @@ extension Guise {
     // MARK: Filter By Container
     
     public static func filter<C: Hashable>(container: C) -> Set<AnyKey> {
-        return filter(type: nil, name: nil, container: container, metathunk: nil)
+        return filter(name: nil, container: container, metathunk: nil)
     }
     
     public static func filter<C: Hashable, M>(container: C, metafilter: @escaping Metafilter<M>) -> Set<AnyKey> {
-        return filter(type: nil, name: nil, container: container, metathunk: metathunk(metafilter))
+        return filter(name: nil, container: container, metathunk: metathunk(metafilter))
     }
     
     public static func filter<C: Hashable, M: Equatable>(container: C, metadata: M) -> Set<AnyKey> {
@@ -157,7 +163,7 @@ extension Guise {
     // MARK: Filter By Metadata
     
     public static func filter<M>(metafilter: @escaping Metafilter<M>) -> Set<AnyKey> {
-        return filter(type: nil, name: nil, container: nil, metathunk: metathunk(metafilter))
+        return filter(name: nil, container: nil, metathunk: metathunk(metafilter))
     }
     
     public static func filter<M: Equatable>(metadata: M) -> Set<AnyKey> {
