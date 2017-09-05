47526726a03ea0458ac49dfe6ef7d049e6f9ff1b
Gregory Higley <code@revolucent.net>
2017-09-05 07:42:09 -0400

    Documentation for `Dependency` along with serialization of cache resolution

--- Guise/Dependency.swift
+++ Guise/Dependency.swift
@@ -8,32 +8,44 @@
 
 import Foundation
 
+/**
+ A simple class to hold the resolution block
+ and a few other interesting bits.
+ */
 class Dependency {
+    /**
+     A private serial dispatch queue for cache resolution.
+     
+     This just ensures that we don't resolve more than once
+     due to concurrency when creating a cached value.
+     */
     private let cacheQueue: DispatchQueue
-    
-    /** Default lifecycle for the dependency. */
+    /// Default lifecycle for the dependency
     let cached: Bool
-    /** Registered block. */
+    /// The registered resolution block
     private let resolution: (Any) -> Any
-    /** Cached instance, if any. */
+    /// Cached instance, if any
     private var instance: Any?
-    /** Metadata */
+    /// Metadata, which defaults to an instance of `Void`, i.e., `()`
     let metadata: Any
     
     init<P, T>(metadata: Any, cached: Bool, resolution: @escaping Resolution<P, T>) {
         self.metadata = metadata
         self.cached = cached
         self.resolution = { param in resolution(param as! P) }
-        self.cacheQueue = DispatchQueue(label: "com.prosumma.Guise.Dependency.\(String(reflecting: T.self)).\(UUID())")
+        let label = "com.prosumma.Guise.Dependency.[\(String(reflecting: T.self))].\(UUID())"
+        self.cacheQueue = DispatchQueue(label: label)
     }
     
+    /// - warning: An incompatible `T` will cause an unrecoverable runtime exception.
     func resolve<T>(parameter: Any, cached: Bool?) -> T {
         var result: T
         if cached ?? self.cached {
-            // Recursion will cause a deadlock here. But that wouldn't be too smart, would it?
-            cacheQueue.sync {
-                if instance != nil { return }
-                instance = resolution(parameter)
+            if instance == nil {
+                cacheQueue.sync {
+                    if instance != nil { return }
+                    instance = resolution(parameter)
+                }
             }
             result = instance! as! T
         } else {
