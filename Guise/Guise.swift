357d14134d6fa3ae12d448434bf886ec3cf04e49
Paul Betts <paul@paulbetts.org>
2017-10-20 14:13:45 -0700

    Silence annoying warnings when using register

--- Guise/Guise.swift
+++ Guise/Guise.swift
@@ -321,7 +321,7 @@ public class ServiceLocator {
          - cached: Whether or not to cache the result of the registration block.
          - resolution: The block to register with Guise.
     */
-    public func register<P, T>(key: Key<T>, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
+    @discardableResult public func register<P, T>(key: Key<T>, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
         lock.write { registrations[AnyKey(key)] = Dependency(metadata: metadata, cached: cached, resolution: resolution) }
         return key
     }
@@ -337,7 +337,7 @@ public class ServiceLocator {
          - cached: Whether or not to cache the result of the registration block.
          - resolution: The block to register with Guise.
     */
-    public func register<P, T>(keys: Set<Key<T>>, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Set<Key<T>> {
+    @discardableResult public func register<P, T>(keys: Set<Key<T>>, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Set<Key<T>> {
         return lock.write {
             for key in keys {
                 registrations[AnyKey(key)] = Dependency(metadata: metadata, cached: cached, resolution: resolution)
@@ -358,7 +358,7 @@ public class ServiceLocator {
         - cached: Whether or not to cache the result of the registration block.
         - resolution: The block to register with Guise.
     */
-    public func register<P, T, N: Hashable, C: Hashable>(name: N, container: C, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
+    @discardableResult public func register<P, T, N: Hashable, C: Hashable>(name: N, container: C, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
         return register(key: Key(name: name, container: container), metadata: metadata, cached: cached, resolution: resolution)
     }
     
@@ -375,7 +375,7 @@ public class ServiceLocator {
      
      - note: The registration is made in the default container, `Name.default`.
      */
-    public func register<P, T, N: Hashable>(name: N, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
+    @discardableResult public func register<P, T, N: Hashable>(name: N, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
         return register(key: Key(name: name, container: Name.default), metadata: metadata, cached: cached, resolution: resolution)
     }
     
@@ -392,7 +392,7 @@ public class ServiceLocator {
      
      - note: The registration is made with the default name, `Name.default`.
      */
-    public func register<P, T, C: Hashable>(container: C, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
+    @discardableResult public func register<P, T, C: Hashable>(container: C, metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
         return register(key: Key(name: Name.default, container: container), metadata: metadata, cached: cached, resolution: resolution)
     }
     
@@ -408,7 +408,7 @@ public class ServiceLocator {
      
      - note: The registration is made in the default container, `Name.default` and under the default name, `Name.default`.
     */
-    public func register<P, T>(metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
+    @discardableResult public func register<P, T>(metadata: Any = (), cached: Bool = false, resolution: @escaping Resolution<P, T>) -> Key<T> {
         return register(key: Key(name: Name.default, container: Name.default), metadata: metadata, cached: cached, resolution: resolution)
     }
     
@@ -423,7 +423,7 @@ public class ServiceLocator {
         - container: The container in which to register the block.
         - metadata: Arbitrary metadata associated with this registration.
     */
-    public func register<T, N: Hashable, C: Hashable>(instance: T, name: N, container: C, metadata: Any = ()) -> Key<T> {
+    @discardableResult public func register<T, N: Hashable, C: Hashable>(instance: T, name: N, container: C, metadata: Any = ()) -> Key<T> {
         return register(key: Key(name: name, container: container), metadata: metadata, cached: true) { instance }
     }
     
@@ -439,7 +439,7 @@ public class ServiceLocator {
      
      - note: The registration is made in the default container, `Name.default`.
     */
-    public func register<T, N: Hashable>(instance: T, name: N, metadata: Any = ()) -> Key<T> {
+    @discardableResult public func register<T, N: Hashable>(instance: T, name: N, metadata: Any = ()) -> Key<T> {
         return register(key: Key(name: name, container: Name.default), metadata: metadata, cached: true) { instance }
     }
     
@@ -455,7 +455,7 @@ public class ServiceLocator {
      
      - note: The registration is made with the default name, `Name.default`.
     */
-    public func register<T, C: Hashable>(instance: T, container: C, metadata: Any = ()) -> Key<T> {
+    @discardableResult public func register<T, C: Hashable>(instance: T, container: C, metadata: Any = ()) -> Key<T> {
         return register(key: Key(name: Name.default, container: container), metadata: metadata, cached: true) { instance }
     }
     
@@ -470,7 +470,7 @@ public class ServiceLocator {
      
      - note: The registration is made with the default name, `Name.default`, and in the default container, `Name.default`.
      */
-    public func register<T>(instance: T, metadata: Any = ()) -> Key<T> {
+    @discardableResult public func register<T>(instance: T, metadata: Any = ()) -> Key<T> {
         return register(key: Key(name: Name.default, container: Name.default), metadata: metadata, cached: true) { instance }
     }
     
